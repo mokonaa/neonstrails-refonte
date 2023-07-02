e886db252613cef5286b5f4e7ef73bd2084118ae
“Aline <hy.aline@outlook.com>
2023-07-02 17:31:24 +0200

    last modifications

--- dist/index.09c24910.js
+++ dist/index.09c24910.js
@@ -1,20 +1,20 @@
 "use strict";
 /**
  * Enable scroll only when the loading of the website is done 
- */ const loadingScreen = document.querySelector(".loading-screen");
+ */ const burgerButton = document.querySelector(".burger-icon");
+const loadingScreen = document.querySelector(".loading-screen");
 document.body.style.overflow = "hidden";
 window.addEventListener("load", ()=>{
-    console.log("loaded !");
     document.body.style.overflow = "auto";
     loadingScreen.classList.add("loading-screen--hide");
+    burgerButton.style.position = "fixed";
 });
 /**
- * Handle clicks on the navbar only on mobile, to hide the navbar
+ * Burger animation fadeIn and fadeOut when burger icon is clicked
  */ // getting all the links and foreach link, if it's clicked the navbar is unchecked
 const allLinksNavbar = document.querySelectorAll(".burger-menu__link");
 const navChecked = document.querySelector("#burger-toggle");
 const navDisplay = document.querySelector(".burger-menu");
-const burgerButton = document.querySelector(".burger-wrapper");
 const closeButton = document.querySelector(".close-icon");
 burgerButton.addEventListener("click", ()=>{
     document.body.style.overflow = "hidden";
@@ -39,37 +39,24 @@ function fadeOut(elementClicked) {
             }, 250);
         }
     });
-} // disable the scroll event for the time the loading is done
- // $(window).on("load", function () {
- //     $(".modal").hide();
- //     $("#neon-text").click(showTrailer);
- //     $("#play-teaser1").click(showTeaser1);
- //     $("#play-teaser2").click(showTeaser2);
- //     $("#play-teaser3").click(showTeaser3);
- //     function showTrailer(evt) {
- //         $("#trailer").show();
- //         $(".close").click(function () {
- //             $("#trailer").hide();
- //         });
- //     }
- //     function showTeaser1(evt) {
- //         $("#modal-teaser1").show();
- //         $(".close").click(function () {
- //             $("#modal-teaser1").hide();
- //         });
- //     }
- //     function showTeaser2(evt) {
- //         $("#modal-teaser2").show();
- //         $(".close").click(function () {
- //             $("#modal-teaser2").hide();
- //         });
- //     }
- //     function showTeaser3(evt) {
- //         $("#modal-teaser3").show();
- //         $(".close").click(function () {
- //             $("#modal-teaser3").hide();
- //         });
- //     }
+}
+/**
+ * Function that handle modals
+ */ // document.querySelector('.welcome__button').addEventListener('click', () => {
+//     document.querySelector('.welcome__modal').showModal();
+// })
+// document.querySelector('.welcome__button--close').addEventListener('click', () => {
+//     document.querySelector('.welcome__modal').close();
+// })
+openModal(".welcome__modal", ".welcome__button", ".welcome__button--close");
+function openModal(modalOpened, openButton, closeButton) {
+    document.querySelector(openButton).addEventListener("click", ()=>{
+        document.querySelector(modalOpened).showModal();
+    });
+    document.querySelector(closeButton).addEventListener("click", ()=>{
+        document.querySelector(modalOpened).close();
+    });
+} // $(window).on("load", function () {
  //     $(window).scroll(function () {
  //         var scroll = $(window).scrollTop();
  //         if (scroll > 400) {
--- src/js/script.js
+++ src/js/script.js
@@ -4,24 +4,25 @@
  * Enable scroll only when the loading of the website is done 
  */
 
+const burgerButton = document.querySelector('.burger-icon');
 const loadingScreen = document.querySelector('.loading-screen');
 document.body.style.overflow = 'hidden';
 
 window.addEventListener('load', () => {
-    console.log('loaded !');
     document.body.style.overflow = 'auto';
     loadingScreen.classList.add('loading-screen--hide');
+    burgerButton.style.position = 'fixed';
+
 });
 
 /**
- * Handle clicks on the navbar only on mobile, to hide the navbar
+ * Burger animation fadeIn and fadeOut when burger icon is clicked
  */
 
 // getting all the links and foreach link, if it's clicked the navbar is unchecked
 const allLinksNavbar = document.querySelectorAll('.burger-menu__link');
 const navChecked = document.querySelector('#burger-toggle');
 const navDisplay = document.querySelector('.burger-menu');
-const burgerButton = document.querySelector('.burger-wrapper');
 const closeButton = document.querySelector('.close-icon');
 
 burgerButton.addEventListener('click', () => {
@@ -38,7 +39,7 @@ allLinksNavbar.forEach((link) => {
 
 function fadeOut(elementClicked) {
     elementClicked.addEventListener('click', () => {
-        if(navDisplay.classList.contains('checked')) {
+        if (navDisplay.classList.contains('checked')) {
             document.body.style.overflow = 'auto';
             navChecked.checked = false;
             navDisplay.style.opacity = '0';
@@ -46,54 +47,35 @@ function fadeOut(elementClicked) {
                 navDisplay.classList.remove('checked');
                 closeButton.style.display = 'none';
             }, 250)
-            
+
         }
     })
 }
 
+/**
+ * Function that handle modals
+ */
 
-// disable the scroll event for the time the loading is done
+// document.querySelector('.welcome__button').addEventListener('click', () => {
+//     document.querySelector('.welcome__modal').showModal();
+// })
+// document.querySelector('.welcome__button--close').addEventListener('click', () => {
+//     document.querySelector('.welcome__modal').close();
+// })
+openModal('.welcome__modal', '.welcome__button', '.welcome__button--close');
+
+function openModal(modalOpened, openButton, closeButton) {
+    document.querySelector(openButton).addEventListener('click', () => {
+        document.querySelector(modalOpened).showModal();
+    })
+    document.querySelector(closeButton).addEventListener('click', () => {
+        document.querySelector(modalOpened).close();
+    })
+}
 
 
 // $(window).on("load", function () {
 
-//     $(".modal").hide();
-//     $("#neon-text").click(showTrailer);
-//     $("#play-teaser1").click(showTeaser1);
-//     $("#play-teaser2").click(showTeaser2);
-//     $("#play-teaser3").click(showTeaser3);
-
-//     function showTrailer(evt) {
-//         $("#trailer").show();
-//         $(".close").click(function () {
-//             $("#trailer").hide();
-//         });
-
-//     }
-
-//     function showTeaser1(evt) {
-//         $("#modal-teaser1").show();
-//         $(".close").click(function () {
-//             $("#modal-teaser1").hide();
-//         });
-
-//     }
-
-//     function showTeaser2(evt) {
-//         $("#modal-teaser2").show();
-//         $(".close").click(function () {
-//             $("#modal-teaser2").hide();
-//         });
-
-//     }
-
-//     function showTeaser3(evt) {
-//         $("#modal-teaser3").show();
-//         $(".close").click(function () {
-//             $("#modal-teaser3").hide();
-//         });
-
-//     }
 
 //     $(window).scroll(function () {
 //         var scroll = $(window).scrollTop();
